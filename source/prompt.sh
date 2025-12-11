2a63453316a7fe1f5a9ffa82746308e59126b6a3
Robin <rehill012@gmail.com>
2025-12-11 16:10:06 -0500

    Fix prompt spacing when not in a git repo

--- source/prompt.sh
+++ source/prompt.sh
@@ -1,9 +1,9 @@
 # Load version control data
 autoload -Uz vcs_info
 precmd() { vcs_info }
-zstyle ':vcs_info:git:*' formats '%b'
+zstyle ':vcs_info:git:*' formats '%b '
 
 # Custom prompt
 # Remove add '1' after '%' to see whole path
 setopt PROMPT_SUBST
-PROMPT='%1~ %F{magenta}${vcs_info_msg_0_}%f > '
+PROMPT='%1~ %F{magenta}${vcs_info_msg_0_}%f> '
